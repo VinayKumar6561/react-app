b21d1a09e63914a06fdd763976ef37510b5358b3
vkurumella <vinayk6561@gmail.com>
2026-06-19 20:04:18 +0530

    Created template with RHF shadcn /UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import Login from "./features/auth/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
